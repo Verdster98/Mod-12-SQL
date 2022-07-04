5b71ccb37e6a57b652fab759ddeaaa7f533bb603
Verdster98 <nicverdi85@gmail.com>
2022-07-04 00:29:43 -0400

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-oogey
+//Dependencies found here
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const cTable = require("console.table");
+const db = require(".");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Morpheus718",
+  database: "employee_info_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+  
+    startScreen();
+    //  connection.end();//
+  });
